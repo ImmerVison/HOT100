643b38640fa804650ea04f5a7a728aee26da0afa
vtlang <shaman2077c@163.com>
2024-03-08 18:56:29 +0800

    :todo:

---
+++ src/main/java/io/goji/leetcode/search2d_matrix/Solution.java
@@ -0,0 +1,12 @@
+package io.goji.leetcode.search2d_matrix;
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int left = 0;
+        int right = m * n - 1;
+        
+    }
+}
